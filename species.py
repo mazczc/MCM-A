94bc7b034c23fb2be8127907d740cf3b77a124ee
mazczc <87382876+mazczc@users.noreply.github.com>
2023-02-19 23:07:51 +0800

    Add files via upload

--- species.py
+++ species.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 
 from multi import *
 
+np.random.seed(1919810)
+
 #####################ARGS#################
 
-B0 = 0.5
+B0 = 8000
 S0 = 0.1
 n = 0.45#cm
 Sw = 0.038#cm
@@ -13,7 +15,7 @@ Ks = 312.77#cm/d
 c = 10
 Zr = 40#cm
 Em = 0.16#cm
-k = 0.38#cm
+k = 0.2#cm
 Yg = 0.9
 R = 0.01
 q = 0.001#/d
@@ -23,7 +25,7 @@ km = 0.85
 Alphamax = 1.1#/d
 Deltamax = 0.109#/d
 
-Rho = 220#cm**2/g
+Rho = 0.022#cm**2/g
 FL = 0.20
 
 Gamma0 =  GetGamma(Rho, FL, Em, n, Zr)
@@ -46,21 +48,42 @@ class species:
 print(AlphaBase, Sw, Gamma0, Beta, 0.85, 2.0, 0.5)
 species_dic = {}
 for i in range(10):
-	s = species(AlphaBase* (i*0.1 + 0.5), Sw* (i*0.1 + 0.5), Gamma0* (i*0.1 + 0.5), Beta* (i*0.1 + 0.5), 0.5 + i*0.05, 2.0, 0.5)
+	s = species(AlphaBase* (i*0.1 + 0.5), Sw* (i*0.035 + 0.3), Gamma0* (i*0.1 + 0.5), Beta* (-i*0.1 + 0.5), 0.85*(i*0.01 + 0.95), 2.0, 0.5)
 	species_dic[i] = s
 
-s = species(1.0, 0.01, 0.20, 0.002, 0.5, 2.0, 0.1)
-species_dic["Xiao Qiang"] = s
 
-s = species(2.0, 0.1, 0.4, 0.004, 0.85, 2.0, 0.5)
-species_dic["Zhang"] = s
 
-s = species()
+s = species(0.0308, 0.01, 1.0018e-5 ,0.0065, 1, 2.0, 0.5)
+species_dic["A"] = s
 
-Species = ["Zhang"]
+s = species(0.051, 0.049, 1.0018e-5 ,0.0145, 1, 2.0, 0.5)
+species_dic["B"] = s
+
+s = species(0.0643, 0.06, 1.5418e-5 ,0.0095, 1, 2.0, 0.5)
+species_dic["C"] = s
+
+s = species(0.0643, 0.06, 2.5418e-5 ,0.0095, 1, 2.0, 0.5)
+species_dic["Cw"] = s
+
+s = species(0.065,0.061,1.5618e-5, 0.0098, 1, 2, 0.5)
+species_dic["D"] = s
+
+s = species(0.075, 0.065, 1.6418e-5 ,0.01, 1, 2.0, 0.5)
+species_dic["E"] = s
+
+s = species(0.08, 0.067, 1.6318e-5, 0.013, 1, 2.0, 0.5)
+species_dic["F"] = s
+
+s = species(0.1,0.072,1.7538e-5,0.009,1,2.0,0.5)
+species_dic["G"] = s
+
+s = species(0.15, 0.081, 2.5418e-5 ,0.0230, 1, 2.0, 0.5)
+species_dic["H"] = s
+ 
+Species = ["B", "B"]
 GammaList = []
 SwList0 = []
-BList0 = []
+BList0 = [B0 / 3, B0*2/3]
 AlphaList0 = []
 AlphamaxList = []
 RLlist = []
@@ -68,7 +91,7 @@ RUlist = []
 kmlist = []
 for choice in Species:
 	# rand = 1
-	BList0.append(B0/len(Species))#np.random.normal(1, 0.3))
+	#BList0.append(B0/len(Species))
 	SwList0.append(species_dic[choice].Sw)
 	GammaList.append(species_dic[choice].Gamma)
 	AlphaList0.append(species_dic[choice].Alpha)
@@ -89,20 +112,33 @@ Rains = [0]
 
 Time = 2000
 DStart = 1000
-DEnd = 1050
-I = [np.random.normal(1.5, 0.1) for i in range(Time)]
+DEnd = 1090
+I = [np.random.normal(0.3, 0.1) for i in range(Time)]
+rand = [np.random.rand() for i in range(Time)]
+droughtrand = [np.random.rand() for i in range(Time)]
 
 for day in range(Time):
 	Rain = I[day]
 	#cm/d
-	if day%5 !=0 :
+	#if rand[day] > 0.202 :
+	#if day%5 != 0:
 	#if day <= 800:
-		Rain=0
+		#Rain=0
 	#I = 0
 	#添加干旱
+	#if day in range(DStart, DEnd):
+		#Rain = 0.1*Rain
 	if day in range(DStart, DEnd):
-		Rain = 0.2*I[day]
-	
+		Rain = 0.2 * Rain
+
+	#if day in range(900, 950):
+	#	Rain = 0.3 * Rain
+
+	#if droughtrand[day] > 0.6:
+	#	Rain = Rain*(1 - droughtrand[day])
+
+	#if day in range(1200, 1250):
+	#	Rain = 0.4*Rain	
 	newBlist = []
 	newAlphaList = []
 	LastRain = Rains[day]
@@ -137,9 +173,10 @@ Rains = [0]
 for day in range(Time):
 	Rain = I[day]
 	#cm/d
-	if day%5 !=0 :
+	#if rand[day] > 0.202 :
+	#if day%5 != 0:
 	#if day <= 800:
-		Rain=0
+		#Rain=0
 	#I = 0
 	#添加干旱
 	#if day in range(DStart, DEnd):
@@ -163,12 +200,21 @@ for day in range(Time):
 	SlistNormal.append(S)
 	Rains.append(Rain)
 
-plt.plot(range(Time + 1), [np.sum(B) for B in BlistNormal],label='Normal'+ str(Species))
+plt.plot(range(Time + 1), [np.sum(B) for B in BlistNormal],label='Total')
+
+for i in range(len(BlistNormal[0])):
+	plt.plot(range(Time+1),[B[i] for B in BlistNormal] , label = 'Type'+str(i))
+
+for i in range(len(BlistDrought[0])):
+	plt.plot(range(Time+1),[B[i] for B in BlistDrought] , label = 'TypeD'+str(i))
 
 Resistent = (np.sum(BlistNormal[DEnd]) - np.sum(BlistDrought[DEnd])) /np.sum(BlistNormal[DEnd])
-Recover = (np.sum(BlistNormal[DEnd + 50]) - np.sum(BlistDrought[DEnd + 50])) /np.sum(BlistNormal[DEnd + 100])
-print("Resistent:", Resistent, ",Recover:", Recover)
-#print(BlistNormal)
+Recover = (np.sum(BlistNormal[DEnd + 100]) - np.sum(BlistDrought[DEnd + 100])) /np.sum(BlistNormal[DEnd + 100])
+print("Resistent:", 1/Resistent, ",Recover:",1/ Recover)
+print(AlphaList)
+
+#plt.plot(range(Time+1),SlistNormal , label = 'S')
 
 plt.legend()
-plt.show()
+plt.savefig("4species-normal-cycle_rain-['Xiao Qiang','D', 'New', 'standard']-with_total.png")
+#plt.show()
